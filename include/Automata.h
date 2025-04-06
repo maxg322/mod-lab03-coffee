0e5a00dba8452b70062b28767fbe21f979289c81
maxg322 <smaxim152@mail.ru>
2025-04-06 20:51:00 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include <map>
+
+class Automata {
+ private:
+    enum STATES {
+        OFF,
+        WAIT,
+        ACCEPT,
+        CHECK,
+        COOK,
+        COLLECT
+    };
+
+    int sizeValue;
+    int selected;
+    int cash;
+    std::string* menu;
+    int* prices;
+    STATES state;
+
+ public:
+    Automata(std::string menu[], int prices[], int size);
+
+    std::string On();
+    std::string Off();
+    std::string Coin(int);
+    std::string etMenu();
+    std::string getState();
+    std::string Choice(int);
+    std::string Check();
+    std::string Cancel();
+    std::string Cook();
+    std::string Finish();
+};
